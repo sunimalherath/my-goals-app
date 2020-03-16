d87b4b48660ea4ddab606e89643688d0ede0170f
cod3breaker <ravisherath@gmail.com>
2020-03-16 19:41:10 +1100

    adding IBOutlets and IBActions to CreateGoalVC

---
+++ my-goals-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,32 @@
+//
+//  CreateGoalVC.swift
+//  my-goals-app
+//
+//  Created by Sunimal Herath on 16/3/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    // IBOutlets
+    @IBOutlet weak var goalTextArea: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // IBActions
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+    }
+    
+}
